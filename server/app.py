6eac6f0c17a8460d86fb2526fe5785986b164594
Albright <albright.muchori@student.moringaschool.com>
2024-01-23 00:38:17 +0300

    models

--- server/app.py
+++ server/app.py
@@ -17,7 +17,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
     API_URL,
     config={  # Swagger UI config overrides
-        'app_name': "Restaurants REST API 1.0"
+        'app_name': "Restaurants REST API "
     }
 )
 
